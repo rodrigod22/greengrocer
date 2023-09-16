94fe9853885c75bcd3d51fdbdb0223c6f58e1c0c
rodrigod22 <rodrigogat5@hotmail.com>
2023-09-16 10:50:41 -0300

    tela SignUp

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          primarySwatch: Colors.green
-          // useMaterial3: true,
-          ),
+        primarySwatch: Colors.green,
+      ),
       debugShowCheckedModeBanner: false,
       home: const SignInScree(),
     );
--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
+import 'package:greengrocer/src/auth/sign_up_screen.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 class SignInScree extends StatelessWidget {
@@ -167,7 +168,12 @@ class SignInScree extends StatelessWidget {
                             color: Colors.green,
                           ),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return const SignUpScreen();
+                          }));
+                        },
                         child: const Text(
                           'Criar conta',
                           style: TextStyle(
