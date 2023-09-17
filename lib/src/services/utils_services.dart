8747df8aae49f3d4cad96e641202eab635f35d55
rodrigod22 <rodrigogat5@hotmail.com>
2023-09-17 19:56:25 -0300

    tela cart

--- lib/src/services/utils_services.dart
+++ lib/src/services/utils_services.dart
@@ -2,7 +2,8 @@ import 'package:intl/intl.dart';
 
 class UtilsServices {
   String priceToCurrency(double price) {
-    NumberFormat numberFormat = NumberFormat.simpleCurrency(locale: 'pt_BR');
+    NumberFormat numberFormat =
+        NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
     return numberFormat.format(price);
   }
 }
